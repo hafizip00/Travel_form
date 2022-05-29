58465b18df6fa4cb754e95e214f15590c24c0391
Inayat <73791952+hafizip00@users.noreply.github.com>
2022-05-29 04:15:17 -0700

    Add files via upload
    Using Bootstrap

---
+++ Travel_Form/app.js
@@ -0,0 +1,67 @@
+// console.log('TRAVEL')
+// const Name = document.getElementById('name');
+// const email = document.getElementById('email');
+// const phone = document.getElementById('phone');
+
+// Name.addEventListener('blur' , ()=>{
+//     console.log('NAME ')
+//     let regex = /[a-zA-Z]([0-9a-zA-Z]){0,10}/
+//     let str = Name.value;
+//     console.log(str, regex)
+//     if(regex.test(str)){
+//         console.log('matched')
+//     }
+//     else{
+
+//     }
+// })
+// email.addEventListener('blur' , ()=>{
+//     console.log('EMAIl ')
+// })
+// phone.addEventListener('blur' , ()=>{
+//     console.log('Phone ')
+// })
+
+
+// const arr = [{name : "apple"} ,{name : "KELO"} ,{name : "ORANGE"} ,{name : "BANANA"}]
+
+// function iterator(array){
+//     let index = 0;
+//     return {
+//         next : function(){
+
+//             if(index < arr.length){
+//                 return {
+//                     value : arr[index++].name,
+//                      done : false
+//                 }
+//             }
+//             else{
+//                return {
+//                     done : true
+//                 }
+//             }  
+//         }
+//     }   
+// }
+// let a = iterator(arr);
+// console.log(a.next().value)
+// console.log(a.next().value)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
